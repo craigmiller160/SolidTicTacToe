cc93e2b479c71a2f11b249e9dd7934443e936617
Craig Miller <craigmiller160@gmail.com>
2022-02-25 10:36:27 -0500

    Progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'path';
 
 export default defineConfig({
+    root: path.join(process.cwd(), 'src'),
     plugins: [solidPlugin()],
     build: {
         target: 'esnext'
