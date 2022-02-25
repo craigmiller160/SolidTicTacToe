52e0f11a1062dbadac9bcb60b26adea94155cc33
Craig Miller <craigmiller160@gmail.com>
2022-02-25 13:11:33 -0500

    Some solid refactoring

---
+++ src/Counter.tsx
@@ -0,0 +1,15 @@
+import { createSignal } from 'solid-js';
+import './style.css';
+
+export const Counter = () => {
+    const [count, setCount] = createSignal(0);
+    const increment = () => setCount(count() + 1);
+
+    return (
+        <div className="Wrapper">
+            <button type="button" onClick={increment}>
+                {count()}
+            </button>
+        </div>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,4 @@
 import { render } from 'solid-js/web';
-import { createSignal } from 'solid-js';
-
-const Counter = () => {
-    const [count, setCount] = createSignal(0);
-    const increment = () => setCount(count() + 1);
-
-    return (
-        <button type="button" onClick={increment}>
-            {count()}
-        </button>
-    );
-};
+import { Counter } from './Counter';
 
 render(() => <Counter />, document.getElementById('root')!);
